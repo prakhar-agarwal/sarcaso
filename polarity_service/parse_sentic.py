ff6149c4a11c52ef8f5e326bb5e90c76f6aa23c4
Apoorv Ingle <apoorv.ingle@gmail.com>
2015-04-06 00:15:27 +0530

    Parsing sentic net successful

--- polarity_service/parse_sentic.py
+++ polarity_service/parse_sentic.py
@@ -1,5 +1,35 @@
 import xml.etree.ElementTree as ET
 
+def add_underscore(s):
+    return s.replace(' ', '_')
+
 db = ET.parse("senticnet3.rdf.xml")
 root = db.getroot()
+
+cl = root.getchildren()
+master_dict = {}
+
+for child in cl:
+    concept = add_underscore(child.find('{http://sentic.net/api}text').text)
+    raw_semantics = child.findall('{http://sentic.net/api}semantics')
+    semantics = []
+    for el in raw_semantics:
+        semantics.append(el.attrib['{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource'].split('/')[-1])
+
+    pleasantness = child.find('{http://sentic.net/api}pleasantness').text
+    attention = child.find('{http://sentic.net/api}attention').text
+    sensitivity = child.find('{http://sentic.net/api}sensitivity').text
+    aptitude = child.find('{http://sentic.net/api}aptitude').text
+    polarity = child.find('{http://sentic.net/api}polarity').text
+
+    master_dict[concept] = {
+            'name': concept,
+            'semantics': semantics, 
+            'pleasantness': pleasantness, 
+            'attention': attention,
+            'sensitivity': sensitivity,
+            'aptitude': aptitude,
+            'polarity': polarity
+        }
+
 print(root)
--- polarity_service/settings.py
+++ polarity_service/settings.py
@@ -1,2 +1,46 @@
-DOMAIN = {'people' : {}}
+MONGO_DBNAME = 'concepts'
 
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE', 'PUT']
+
+ACHE_CONTROL = 'max-age=20'
+CACHE_EXPIRES = 20
+
+concepts = {
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'name'
+    },
+    'schema': {
+        'name': {
+            'type': 'string',
+            'required': True,
+            'unique': True,
+        },
+        'semantics':{
+            'type': 'list'
+        },
+        'pleasantness': {
+            'type': 'string',
+        },
+        'attention': {
+            'type': 'string',
+        },
+        'polarity': {
+            'type': 'string',
+        },
+        'aptitude': {
+            'type': 'string',
+        },
+        'sensitivity': {
+            'type': 'string',
+        },
+    }
+}
+
+DOMAIN = {
+    'concepts': concepts,
+}
